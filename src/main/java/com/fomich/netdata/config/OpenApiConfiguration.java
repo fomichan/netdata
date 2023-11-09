c3a4407ef56d2b6d7acca2a660a092604ecf0e05
fomich.alexey <fomich.alexey@yandex.ru>
2023-11-09 23:28:35 +0100

    OAuth2 by google added

---
+++ src/main/java/com/fomich/netdata/config/OpenApiConfiguration.java
@@ -0,0 +1,25 @@
+package com.fomich.netdata.config;
+
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+
+
+// Swagger configuration
+@Configuration
+@SecurityScheme(
+        name = "oauth2",
+        type = SecuritySchemeType.OAUTH2,
+        flows = @OAuthFlows(
+                authorizationCode = @OAuthFlow(
+                        authorizationUrl = "http://localhost:8080/oauth2/authorization/google",
+                        tokenUrl = "https://www.googleapis.com/oauth2/v4/token"
+                )
+        )
+)
+public class OpenApiConfiguration {
+}
--- src/main/java/com/fomich/netdata/config/SecurityConfiguration.java
+++ src/main/java/com/fomich/netdata/config/SecurityConfiguration.java
@@ -1,6 +1,8 @@
 package com.fomich.netdata.config;
 
 import com.fomich.netdata.database.entity.Role;
+import com.fomich.netdata.service.security.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
@@ -9,16 +11,28 @@ import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Set;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 
 @Configuration
 @EnableMethodSecurity // чтобы работали security аннотации над методами (контроллеров)
+@RequiredArgsConstructor
 public class SecurityConfiguration{
+
+    private final UserService userService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -31,7 +45,6 @@ public class SecurityConfiguration{
                         //.requestMatchers("/users/**").hasAuthority(Role.MANAGER.getAuthority())
                         .anyRequest().authenticated() //любое обращение к url только с authenticated пользователем
                 )
-
                 .logout(logout -> logout
                         .logoutUrl("/logout")
                         .logoutSuccessUrl("/login") // в случае успешного logout (по умолчанию так)
@@ -42,6 +55,13 @@ public class SecurityConfiguration{
                         .defaultSuccessUrl("/multiplexers", true) // если прошла успешно, идти туда Установите второй аргумент как true для метода GET
                         //.successForwardUrl("/multiplexers")
                         //.permitAll() // всем доступна страничка login
+
+                )
+                .oauth2Login(oauth2Login ->
+                        oauth2Login
+                                .loginPage("/login")
+                                .defaultSuccessUrl("/multiplexers")
+                                .userInfoEndpoint(userInfo -> userInfo.oidcUserService(oidcUserService())) // Он отвечает за создание объекта authentication (изменим его Чтобы совместить существующих пользователей с Oauth). Нужно предоставить свою реализацию oidcUserService
                 );
                 //.httpBasic(withDefaults());
         return http.build(); // возвращает securityFilterChain объект
@@ -49,12 +69,34 @@ public class SecurityConfiguration{
 
 
 
+    private OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() { // OAuth2UserService<OidcUserRequest, OidcUser> это функциональный интерфейс который принимает OidcUserRequest и возвращает OidcUser
+        return userRequest -> {
+            String email = userRequest.getIdToken().getClaim("email"); //idToken это JWT токен, все значения уже распаршены. Передачу email мы настроили в scope провайдера: просили openid, email, profile
+            // сайт jwt.io позволяет посмотреть что в этом токене
+            // userRequest.getAccessToken() есть также access token от oauth провайдера с ролями, но мы эти роли не используем
 
-    // Создадим password encoder
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+            // TODO: 24.04.2022 create user userService.create
+            UserDetails userDetails = userService.loadUserByUsername(email); // достанем пользователя из БД
+//            new OidcUserService().loadUser()
+            DefaultOidcUser oidcUser = new DefaultOidcUser(userDetails.getAuthorities(), userRequest.getIdToken()); // создадим нового oidc user с нашими authorities
+
+            Set<Method> userDetailsMethods = Set.of(UserDetails.class.getMethods());
+
+            // Вернем proxy который если мы обращаемся к методу userDetails возвращает данные userDetails объекта, если обращаемся к методу oidcUser то из oidcUser объекта
+            return (OidcUser) Proxy.newProxyInstance(SecurityConfiguration.class.getClassLoader(),
+                    new Class[]{UserDetails.class, OidcUser.class}, // набор интерфейсов
+                    (proxy, method, args) -> userDetailsMethods.contains(method) // если запрашиваемый метод есть в userDetailsMethods
+                            ? method.invoke(userDetails, args) // тогда вызываем у userDetails
+                            : method.invoke(oidcUser, args)); // иначе у oidcUser
+        };
     }
+
+
+
+
+
+
+
 
     /*
     @Bean
